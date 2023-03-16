unc-enap/Orca

commit aa687fdbbcc271dd33970133bf965a08f2a94ad6

jfw - sync of Mark's changes consistent with L200 code

Source/Objects/Hardware/Internet/FlashCam/FlashCamBaseClasses/ORFlashCamCard.m
@@ -36,7 +36,7 @@ NSString* ORFlashCamCardSettingsLock            = @"ORFlashCamCardSettingsLock";
 
 - (id) init
 {
-    [super init];
+    self = [super init]; //MAH 9/18/22 need to assign self to init 
     [[self undoManager] disableUndoRegistration];
     [self setCardAddress:0];
     [self setPROMSlot:0];
@@ -76,10 +76,12 @@ NSString* ORFlashCamCardSettingsLock            = @"ORFlashCamCardSettingsLock";
 
 - (void) releaseFirmwareVer
 {
-    if(firmwareVer){
-        for(NSUInteger i=0; i<[firmwareVer count]; i++) [[firmwareVer objectAtIndex:i] release];
-        [firmwareVer release];
-    }
+//    if(firmwareVer){
+//        for(NSUInteger i=0; i<[firmwareVer count]; i++) [[firmwareVer objectAtIndex:i] release];
+//        [firmwareVer release];
+//    }
+    
+    [firmwareVer release]; //MAH 9/18/22 no need to release each object in array
     firmwareVer = nil;
     [[NSNotificationCenter defaultCenter] postNotificationName:ORFlashCamCardFirmwareVerChanged object:self];
 }
@@ -409,17 +411,17 @@ NSString* ORFlashCamCardSettingsLock            = @"ORFlashCamCardSettingsLock";
 - (void) taskData:(NSDictionary*)taskData
 {
     if(!taskData) return;
-    NSString* text = [[taskData objectForKey:@"Text"] retain];
+    NSString* text = [taskData objectForKey:@"Text"]; //MAH 9/18/22 no need for the retain??
     if(!text) return;
-    if(!taskdata) taskdata = [[NSMutableArray array] retain];
-    [taskdata addObject:[[NSString stringWithString:text] retain]];
+    //if(!taskdata) taskdata = [[NSMutableArray array] retain];//MAH 9/18/22 if taskData==nil won't get here
+    [taskdata addObject:[NSString stringWithString:text]]; //MAH 9/18/22 no need for retain
     NSRange r = [text rangeOfString:@"ORFlashCamCard"];
     if(r.location != NSNotFound){
         NSString* s = [text substringWithRange:NSMakeRange(r.location, [text length]-r.location)];
         r = [s rangeOfString:@" endl "];
         if(r.location != NSNotFound) NSLog(@"%@\n", [s substringWithRange:NSMakeRange(0, r.location)]);
     }
-    [text release];
+    //[text release];//MAH 9/18/22 no longer retained above
 }
 
 // Once the firmware version request task is complete, release the old firmware version
@@ -432,10 +434,11 @@ NSString* ORFlashCamCardSettingsLock            = @"ORFlashCamCardSettingsLock";
         [self releaseFirmwareVer];
         return;
     }
-    if(firmwareVer){
-        for(NSUInteger i=0; i<[firmwareVer count]; i++) [[firmwareVer objectAtIndex:i] release];
-        [firmwareVer release];
-    }
+//    if(firmwareVer){
+//        for(NSUInteger i=0; i<[firmwareVer count]; i++) [[firmwareVer objectAtIndex:i] release];
+//        [firmwareVer release];
+//    }
+    [firmwareVer release]; //MAH 9/18/22 no need to release each object, just the array
     firmwareVer = nil;
     if(taskdata){
         NSString* s = [taskdata componentsJoinedByString:@" "];
@@ -447,11 +450,12 @@ NSString* ORFlashCamCardSettingsLock            = @"ORFlashCamCardSettingsLock";
             if([td count] <= idx+15) return;
             if([[td objectAtIndex:idx+1] isEqualToString:@"found"] &&
                 [[td objectAtIndex:idx+4] containsString:[NSString stringWithFormat:@"%d/0x",cardAddress]]){
-                NSString* fwt = [[NSString stringWithString:[td objectAtIndex:idx+7]] retain];
-                NSString* fwr = [[NSString stringWithString:[td objectAtIndex:idx+9]] retain];
-                NSString* fwd = [[NSString stringWithFormat:@"%@ %@ %@",
-                                 [td objectAtIndex:idx+12], [td objectAtIndex:idx+11], [td objectAtIndex:idx+13]] retain];
-                NSString* fwg = [[NSString stringWithString:[td objectAtIndex:idx+15]] retain];
+                NSString* fwt = [NSString stringWithString:[td objectAtIndex:idx+7]];//MAH 9/18/22 no need for retain
+                NSString* fwr = [NSString stringWithString:[td objectAtIndex:idx+9]];//MAH 9/18/22 no need for retain
+                NSString* fwd = [NSString stringWithFormat:@"%@ %@ %@",
+                                 [td objectAtIndex:idx+12], [td objectAtIndex:idx+11], [td objectAtIndex:idx+13]];//MAH 9/18/22 no need for retain
+                NSString* fwg = [NSString stringWithString:[td objectAtIndex:idx+15]];//MAH 9/18/22 no need for retain
+                [firmwareVer release];
                 firmwareVer = [[NSArray arrayWithObjects:fwt, fwr, fwd, fwg, nil] retain];
                 if([td count] >= idx+17){
                     if([[td objectAtIndex:idx+17] isEqualToString:@"Booting"]){

Orca.xcodeproj/project.pbxproj
@@ -19944,6 +19944,7 @@
 				);
 				LIBRARY_SEARCH_PATHS_QUOTED_FOR_TARGET_1 = "\"$(SRCROOT)/Libraries/libUniversal\"";
 				LIBRARY_SEARCH_PATHS_QUOTED_FOR_TARGET_2 = "\"$(SRCROOT)/Libraries\"";
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				OTHER_CFLAGS = "";
 				OTHER_CPLUSPLUSFLAGS = "";
 				OTHER_LDFLAGS = (
@@ -20027,6 +20028,7 @@
 				);
 				LIBRARY_SEARCH_PATHS_QUOTED_2 = "\"$(SRCROOT)/Libraries\"";
 				LIBRARY_SEARCH_PATHS_QUOTED_FOR_TARGET_1 = "\"$(SRCROOT)/Libraries/libUniversal\"";
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				OTHER_CFLAGS = "";
 				OTHER_CPLUSPLUSFLAGS = "";
 				OTHER_LDFLAGS = (
@@ -20106,6 +20108,7 @@
 				);
 				LIBRARY_SEARCH_PATHS_QUOTED_2 = "\"$(SRCROOT)/Libraries\"";
 				LIBRARY_SEARCH_PATHS_QUOTED_FOR_TARGET_1 = "\"$(SRCROOT)/Libraries/libUniversal\"";
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				OTHER_CFLAGS = "";
 				OTHER_CPLUSPLUSFLAGS = "";
 				OTHER_LDFLAGS = (
@@ -20176,7 +20179,7 @@
 				GCC_WARN_UNUSED_VALUE = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				LINK_WITH_STANDARD_LIBRARIES = YES;
-				MACOSX_DEPLOYMENT_TARGET = 10.12;
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				ONLY_ACTIVE_ARCH = YES;
 				OTHER_LDFLAGS = "";
 				SDKROOT = "";
@@ -20222,7 +20225,7 @@
 				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
 				GCC_WARN_UNKNOWN_PRAGMAS = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
-				MACOSX_DEPLOYMENT_TARGET = 10.12;
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				ONLY_ACTIVE_ARCH = YES;
 				OTHER_LDFLAGS = "";
 				SDKROOT = "";
@@ -20268,7 +20271,7 @@
 				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
 				GCC_WARN_UNKNOWN_PRAGMAS = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
-				MACOSX_DEPLOYMENT_TARGET = 10.12;
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				ONLY_ACTIVE_ARCH = YES;
 				OTHER_LDFLAGS = "";
 				SDKROOT = "";

Source/Objects/Hardware/Internet/FlashCam/FlashCamADC/ORFlashCamADCModel.m
@@ -118,6 +118,7 @@ NSString* ORFlashCamADCModelBaselineSampleTimeChanged    = @"ORFlashCamADCModelB
     [cimage drawInRect:rect fromRect:NSZeroRect operation:NSCompositingOperationSourceOver fraction:1.0];
     [image unlockFocus];
     [self setImage:image];
+    [image release]; //MAH 2/18/22
 }
 
 - (void) makeMainController

Source/Objects/Hardware/Internet/FlashCam/FlashCamGlobalTrigger/ORFlashCamGlobalTriggerModel.m
@@ -56,6 +56,7 @@ NSString* ORFlashCamGlobalTriggerModelMajorityWidthChanged = @"ORFlashCamGlobalT
     [cimage drawInRect:rect fromRect:NSZeroRect operation:NSCompositingOperationSourceOver fraction:1.0];
     [image unlockFocus];
     [self setImage:image];
+    [image release]; //MAH 2/18/22
 }
 
 - (void) makeMainController

Source/Objects/Hardware/Internet/FlashCam/FlashCamIO/ORFlashCamListenerModel.h
@@ -69,12 +69,14 @@
     ORTimeRate* dataRateHistory;
     ORTimeRate* eventRateHistory;
     ORTimeRate* deadTimeHistory;
-    ORTaskSequence* runTask;
+    ORTaskSequence* taskSequencer;  //changed name MAH 9/17/22
+    NSTask*     runTask;            //added. MAH 9/17/22
     ORReadOutList* readOutList;
     NSArray* dataTakers;
     NSMutableArray* readOutArgs;
     NSMutableArray* chanMap;
     NSMutableArray* cardMap;
+    NSLock* readStateLock; //MAH 9/18/22
 }
 
 #pragma mark •••Initialization
@@ -115,7 +117,7 @@
 - (ORTimeRate*) dataRateHistory;
 - (ORTimeRate*) eventRateHistory;
 - (ORTimeRate*) deadTimeHistory;
-- (ORTaskSequence*) runTask;
+- (ORTaskSequence*) taskSequencer;
 - (ORReadOutList*) readOutList;
 - (NSMutableArray*) readOutArgs;
 - (NSMutableArray*) children;

Source/Objects/Hardware/Internet/FlashCam/FlashCamReadout/ORFlashCamReadoutModel.m
@@ -112,6 +112,7 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
     [cimage drawInRect:rect fromRect:NSZeroRect operation:NSCompositingOperationSourceOver fraction:1.0];
     [image unlockFocus];
     [self setImage:image];
+    [image release]; //MAH 2/18/22
 }
 
 - (void) makeConnectors
@@ -293,7 +294,7 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
     if(!eth) return;
     if(!ethInterface) ethInterface = [[NSMutableArray array] retain];
     if([self indexOfInterface:eth] >= 0) return;
-    [ethInterface addObject:[eth copy]];
+    [ethInterface addObject:[[eth copy]autorelease]];
     [ethType addObject:@"efb1"];
     if([self ethInterfaceCount] <= kFlashCamMaxEthInterfaces){
         int i = [self ethInterfaceCount] - 1;
@@ -308,8 +309,9 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
     if(!eth) return;
     if(index < 0 || index >= [self ethInterfaceCount]) return;
     if([self indexOfInterface:eth] == index) return;
-    NSString* tmp = [[ethInterface objectAtIndex:index] copy];
-    [[ethInterface objectAtIndex:index] autorelease];
+    NSString* tmp = [ethInterface objectAtIndex:index];
+    //[[ethInterface objectAtIndex:index] autorelease];//MAH 9/18/22 no need for this
+    [ethInterface removeObjectAtIndex:index];//MAH 9/18/22 this should do it  
     if([self indexOfInterface:eth] < 0){
         [ethInterface setObject:eth atIndexedSubscript:index];
         for(int i=0; i<[self listenerCount]; i++){
@@ -369,7 +371,7 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
     if(index < 0 || index >= [self ethInterfaceCount]) return;
     for(int i=1; i<=kFlashCamEthTypeCount; i++){
         if([eth isEqualToString:[NSString stringWithFormat:@"efb%d",i]]){
-            [ethType setObject:[eth copy] atIndexedSubscript:index];
+            [ethType setObject:[[eth copy]autorelease] atIndexedSubscript:index];
             [[NSNotificationCenter defaultCenter] postNotificationName:ORFlashCamReadoutModelEthTypeChanged object:self];
             break;
         }
@@ -405,11 +407,12 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
         NSLog(@"ORFlashCamReadoutModel: cannot add listener with identical interface %@ and port %d\n", eth, (int)p);
         return;
     }
-    ORFlashCamListenerModel* l = [[[ORFlashCamListenerModel alloc] initWithInterface:eth port:p] retain];
+    ORFlashCamListenerModel* l = [[ORFlashCamListenerModel alloc] initWithInterface:eth port:p];
     [self addObject:l];
     NSDictionary* info = [NSDictionary dictionaryWithObjectsAndKeys:
                           [NSNumber numberWithInt:[self listenerCount]-1],
                           @"index", [NSNumber numberWithInt:(int)[l tag]], @"tag", nil];
+    [l release]; //MAH 9/18/22
     [[NSNotificationCenter defaultCenter] postNotificationName:ORFlashCamReadoutModelListenerAdded object:info];
 }
 
@@ -424,7 +427,7 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
     }
     ORFlashCamListenerModel* l = [self getListenerAtIndex:i];
     if(!l){
-        l = [[[ORFlashCamListenerModel alloc] initWithInterface:eth port:p] retain];
+        l = [[[ORFlashCamListenerModel alloc] initWithInterface:eth port:p]autorelease];
         [[self orcaObjects] setObject:l atIndexedSubscript:i];
         NSDictionary* info = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:i],
                               @"index", [NSNumber numberWithInt:(int)[l tag]], @"tag", nil];
@@ -448,7 +451,7 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
 {
     if(i < 0 || i >= [self listenerCount]) return;
     int t = (int) [[self getListenerAtIndex:i] tag];
-    [[[self orcaObjects] objectAtIndex:i] autorelease];
+    //[[[self orcaObjects] objectAtIndex:i] autorelease];//MAH 9/18/22 removing the object in the next line will release
     [[self orcaObjects] removeObjectAtIndex:i];
     NSDictionary* info = [NSDictionary dictionaryWithObjectsAndKeys:
                           [NSNumber numberWithInt:i], @"index", [NSNumber numberWithInt:t], @"tag", nil];
@@ -567,8 +570,8 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
 
 - (void) taskData:(NSDictionary*)taskData
 {
-    id        task = [[taskData objectForKey:@"Task"] retain];
-    NSString* text = [[taskData objectForKey:@"Text"] retain];
+    id        task = [taskData objectForKey:@"Task"];//MAH 9/18/22 no need for retain??
+    NSString* text = [taskData objectForKey:@"Text"];//MAH 9/18/22 no need for retain??
     if(task == pingTask){
         if([text rangeOfString:@" 0.0% packet loss"].location != NSNotFound) pingSuccess = YES;
         else pingSuccess = NO;
@@ -586,8 +589,7 @@ static NSString* ORFlashCamReadoutModelEthConnectors[kFlashCamMaxEthInterfaces] 
         if(prev != validFCSourcePath)
             [[NSNotificationCenter defaultCenter] postNotificationName:ORFlashCamReadoutModelFCSourcePathChanged object:self];
     }
-    [task release];
-    [text release];
+    //MAH 9/18/22 removed releases
 }
 
 - (int) ethIndexForCard:(ORCard*)card

commit f8b678b5c2ffbff1cd4451b5320c83b6b3892f56

Make the end of run delay for the FlashCam listener depend on the log time setting to avoid read failures. Fix bug in getting the FlashCam card firmware version that was introduced in commit aa687fd.

Source/Objects/Hardware/Internet/FlashCam/FlashCamBaseClasses/ORFlashCamCard.m
@@ -467,7 +467,8 @@ NSString* ORFlashCamCardSettingsLock            = @"ORFlashCamCardSettingsLock";
     if(!taskData) return;
     NSString* text = [taskData objectForKey:@"Text"]; //MAH 9/18/22 no need for the retain??
     if(!text) return;
-    [taskdata addObject:[NSString stringWithString:text]]; //MAH 9/18/22 no need for retain
+    if(!taskdata) taskdata = [[[NSMutableArray alloc] init] retain];
+    [taskdata addObject:[NSString stringWithString:text]];
     NSRange r = [text rangeOfString:@"ORFlashCamCard"];
     if(r.location != NSNotFound){
         NSString* s = [text substringWithRange:NSMakeRange(r.location, [text length]-r.location)];
@@ -514,8 +515,8 @@ NSString* ORFlashCamCardSettingsLock            = @"ORFlashCamCardSettingsLock";
                 *stop = YES;
             }
         }];
-        for(id obj in taskdata) [obj release];
-        [taskdata removeAllObjects];
+        [taskdata release];
+        taskdata = nil;
     }
     [[NSNotificationCenter defaultCenter] postNotificationName:ORFlashCamCardFirmwareVerChanged object:self];
 }

Source/Objects/Hardware/Internet/FlashCam/FlashCamIO/ORFlashCamListenerModel.m
@@ -1674,7 +1674,7 @@ NSString* ORFlashCamListenerModelFCRunLogFlushed     = @"ORFlashCamListenerModel
         readWait = true;
         NSFileHandle*  fh = [[runTask standardInput] fileHandleForWriting];
         [fh writeData:[@"\n" dataUsingEncoding: NSASCIIStringEncoding]];
-        [ORTimer delay:1.9]; //been told we have to wait 2 seconds
+        [ORTimer delay:[[self configParam:@"logTime"] doubleValue]*2.1];
         readWait = false;
         [readStateLock lock];
         timeToQuitReadoutThread = YES;