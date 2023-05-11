9674a6dd2faaec728a593563aa40ff54643e64d3
Mark Howe <mhowe@physics.unc.edu>
2023-05-11 16:58:39 -0400

    Changed to Apple's new recommended singleton code. This clears ~190 false positives for leaks in the static analyzer.

--- Source/Misc/SynthesizeSingleton.h
+++ Source/Misc/SynthesizeSingleton.h
@@ -6,9 +6,9 @@ static OR##classname* shared##classname = nil; \
 + (OR##classname*) shared##classname \
 { \
 @synchronized(self) { \
-if (shared##classname == nil) { \
-[[self alloc] init]; \
-} \
+    if (shared##classname == nil) { \
+        shared##classname = [[super allocWithZone:NULL] init];\
+    } \
 } \
 \
 return shared##classname; \
@@ -16,38 +16,9 @@ return shared##classname; \
 \
 + (id)allocWithZone:(NSZone *)zone \
 { \
-@synchronized(self) { \
-if (shared##classname == nil) { \
-shared##classname = [super allocWithZone:zone]; \
-return shared##classname; \
-} \
-} \
-\
-return nil; \
-} \
-\
-- (id)copyWithZone:(NSZone *)zone \
-{ \
-return self; \
-} \
-\
-- (id)retain \
-{ \
-return self; \
-} \
-\
-- (NSUInteger)retainCount \
-{ \
-return 0xffffffff; \
-} \
-\
-- (oneway void)release \
-{ \
-} \
-\
-- (id)autorelease \
-{ \
-return self; \
+    @synchronized(self) { \
+        return [[self shared##classname] retain];\
+    } \
 }
 
 #define SYNTHESIZE_SINGLETON_FOR_CLASS(classname) \
@@ -56,47 +27,18 @@ static classname* shared##classname = nil; \
 \
 + (classname*) shared##classname \
 { \
-@synchronized(self) { \
-if (shared##classname == nil) { \
-[[self alloc] init]; \
-} \
-} \
-\
-return shared##classname; \
-} \
+    @synchronized(self) { \
+        if (shared##classname == nil) { \
+            shared##classname = [[super allocWithZone:NULL] init];\
+        } \
+        return shared##classname; \
+    } \
+}\
 \
 + (id)allocWithZone:(NSZone *)zone \
 { \
-@synchronized(self) { \
-if (shared##classname == nil) { \
-shared##classname = [super allocWithZone:zone]; \
-return shared##classname; \
-} \
-} \
-\
-return nil; \
-} \
-\
-- (id)copyWithZone:(NSZone *)zone \
-{ \
-return self; \
-} \
-\
-- (id)retain \
-{ \
-return self; \
-} \
-\
-- (NSUInteger)retainCount\
-{ \
-return 0xffffffff; \
-} \
-\
-- (oneway void)release \
-{ \
-} \
-\
-- (id)autorelease \
-{ \
-return self; \
-}
+    @synchronized(self) { \
+        return [[self shared##classname] retain];\
+    } \
+    return nil; \
+}\
