1ad13ae4b2cd4555341ec3b3c01647b370becfe2
himal1 <hi123himal@gmail.com>
2025-01-16 08:40:55 -0500

    Removed Unused variable sourcePositions

--- Source/Experiments/LEGEND/L200/ORL200Model.m
+++ Source/Experiments/LEGEND/L200/ORL200Model.m
@@ -1385,12 +1385,6 @@ NSString* ORL200ModelMetaErrorChanged    = @"ORL200ModelMetaErrorChanged";
         [keys addObject:dataFileName];
         [positionDict setObject:[NSNumber numberWithUnsignedLong:[keys count]] forKey:@"number_of_keys"];
 
-        //--------------------------------
-        //put in the source array
-        NSMutableDictionary* sourcePositions = [positionDict objectForKey:@"positions_in_mm"];
-        if(!sourcePositions){
-            sourcePositions = [NSMutableDictionary dictionary];
-        }
         [positionDict setObject:[self makeSourceDictionary:currentSourceArray] forKey:@"positions_in_mm"];
     }
 
